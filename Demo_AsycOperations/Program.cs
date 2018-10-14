68799c97aa96d86335e0b9ad36ecc6f01375a542
John Velis <john@johnvelis.com>
2018-10-14 11:54:15 -0400

    initial push

--- Demo_AsycOperations/Program.cs
+++ Demo_AsycOperations/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Demo_AsycOperations
@@ -10,6 +11,31 @@ namespace Demo_AsycOperations
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Demonstration of Asynchronous Operations");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to list factorials up to 20.");
+            Console.ReadKey();
+
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine(Factorial(i));
+            }
+
+            Console.ReadKey();
+        }
+
+        static double Factorial(double operand)
+        {
+            if (operand != 1)
+            {
+                operand = operand * Factorial(operand - 1);
+            }
+
+            Thread.Sleep(75);
+
+            return (operand);
         }
     }
 }
