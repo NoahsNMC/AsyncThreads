c082a1e142f5bfe5a3047bd8f9d71fb57ac05d61
John Velis <john@johnvelis.com>
2018-10-15 17:28:35 -0400

    50!

--- Demo_AsycOperations/Program.cs
+++ Demo_AsycOperations/Program.cs
@@ -11,24 +11,30 @@ namespace Demo_AsycOperations
     {
         static void Main(string[] args)
         {
-            double factorial1, factorial2;
+            double factorial1, factorial2, factorial3;
 
             Console.WriteLine("Demonstration of Asynchronous Operations");
 
             Console.WriteLine();
 
-            Console.WriteLine("Press any key to calculate 20! and 30!.");
+            Console.WriteLine("Press any key to calculate 20!, 30!, and 50!.");
             Console.ReadKey();
             Console.WriteLine("Calculating");
 
+            //factorial1 = Factorial(20);
+            //factorial2 = Factorial(30);
+            //factorial3 = Factorial(50);
 
             Task<double> factorialTask1 = GetFactorialAsync(20);
-            factorial1 = factorialTask1.Result;
             Task<double> factorialTask2 = GetFactorialAsync(30);
+            Task<double> factorialTask3 = GetFactorialAsync(50);
+            factorial1 = factorialTask1.Result;
             factorial2 = factorialTask2.Result;
+            factorial3 = factorialTask3.Result;
 
             Console.WriteLine($"20! = {factorial1}");
             Console.WriteLine($"30! = {factorial2}");
+            Console.WriteLine($"50! = {factorial3}");
 
             Console.WriteLine("Press any key to continue.");
             Console.ReadKey();
